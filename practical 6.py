749a4221831e9c38630c8a719447655ebc2ba301
sunidhiwath <sunidhiwath.cse24@sbjit.edu.in>
2026-03-11 22:39:30 +0530

    Add files via upload

---
+++ practical 6.py
@@ -0,0 +1,28 @@
+s = input("Enter a string: ")
+
+vowels = 0
+consonants = 0
+spaces = 0
+lowercase = 0
+
+for ch in s:
+    if ch in "aeiouAEIOU":
+        vowels += 1
+    elif ch.isalpha():
+        consonants += 1
+    
+    if ch == " ":
+        spaces += 1
+    
+    if ch.islower():
+        lowercase += 1
+
+print("Number of Vowels:", vowels)
+print("Number of Consonants:", consonants)
+print("Number of Spaces:", spaces)
+print("Number of Lowercase Letters:", lowercase)
+line = input("Enter a sentence: ")
+
+result = line.upper()
+
+print("Output:", result)
